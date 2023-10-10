5f79cd55392d3a07d9048fe2ae402819423e4926
Patrik Brosell <patrikbrosell@gmail.com>
2023-10-10 00:54:06 +0000

    Introduce gamestate
    Introduce enum for pieces
    Introduce struct for Square representing a square on the board

--- game.c
+++ game.c
@@ -7,6 +7,25 @@
 
 static void MainLoop(void);
 
+typedef enum pieces {
+	EMPTY,
+	KING,
+	QUEEN,
+	ROOK,
+	BISHOP,
+	KNIGHT,
+	PAWN,
+} pieces;
+
+typedef struct Square {
+	int x;
+	int y;
+	int piece;
+	int color;
+} Square;
+
+Square g_game[8][8] = {0};	// Game state
+
 int g_screenWidth = 950;
 int g_screenHeight = 950;
 
