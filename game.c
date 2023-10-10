a17a3101e8865a41996b45cd57250850d8bdd389
Patrik Brosell <patrikbrosell@gmail.com>
2023-10-10 01:00:43 +0000

    Create procedure to initialize game state

--- game.c
+++ game.c
@@ -6,6 +6,7 @@
 #endif
 
 static void MainLoop(void);
+static void InitGame(void); // initialize game state
 
 typedef enum pieces {
 	EMPTY,
@@ -31,6 +32,8 @@ int g_screenHeight = 950;
 
 int main(void)
 {
+	InitGame();
+
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 
 	InitWindow(g_screenWidth, g_screenHeight, "window title name");
@@ -48,6 +51,37 @@ int main(void)
 
 	CloseWindow();	// Close window and unload OpenGL context
 	return 0;
+}
+
+void InitGame(void)
+{
+	for(int i = 0; i < 8; ++i) {
+		for(int j = 0; j < 8; ++j) {
+			g_game[i][j].x = i;
+			g_game[i][j].y = j;
+			g_game[i][j].piece = EMPTY;
+		}
+	}
+	g_game[0][0].piece = ROOK;
+	g_game[1][0].piece = KNIGHT;
+	g_game[2][0].piece = BISHOP;
+	g_game[3][0].piece = QUEEN;
+	g_game[4][0].piece = KING;
+	g_game[5][0].piece = BISHOP;
+	g_game[6][0].piece = KNIGHT;
+	g_game[7][0].piece = ROOK;
+	for(int i = 0; i < 8; ++i) {
+		g_game[i][1].piece = PAWN;
+		g_game[i][6].piece = PAWN;
+	}
+	g_game[0][7].piece = ROOK;
+	g_game[1][7].piece = KNIGHT;
+	g_game[2][7].piece = BISHOP;
+	g_game[3][7].piece = QUEEN;
+	g_game[4][7].piece = KING;
+	g_game[5][7].piece = BISHOP;
+	g_game[6][7].piece = KNIGHT;
+	g_game[7][7].piece = ROOK;
 }
 
 void MainLoop(void)
