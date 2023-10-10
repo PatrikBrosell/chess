125aa0c39a1475812ef1f17c7be82865e57cbe63
Patrik Brosell <patrikbrosell@gmail.com>
2023-10-10 00:02:50 +0000

    We have an (empty) window

--- game.c
+++ game.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
+#include <raylib.h>
+
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>
+#endif
+
+static void MainLoop(void);
+
+int g_screenWidth = 950;
+int g_screenHeight = 950;
 
 int main(void)
 {
-	printf("Chess!\n");
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+
+	InitWindow(g_screenWidth, g_screenHeight, "window title name");
+
+#if defined(PLATFORM_WEB)
+	emscripten_set_main_loop(MainLoop, 0, 1);
+#else
+
+	SetTargetFPS(60);
+	while (!WindowShouldClose())
+	{
+		MainLoop();
+	}
+#endif // PLATFORM_WEB
+
+	CloseWindow();	// Close window and unload OpenGL context
 	return 0;
+}
+
+void MainLoop(void)
+{
+	BeginDrawing();
+	EndDrawing();
 }
