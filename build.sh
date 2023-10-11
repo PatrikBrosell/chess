4a1434f1db247406a995f89f3285fe08fe04da2b
Patrik Brosell <patrikbrosell@gmail.com>
2023-10-11 16:25:53 +0000

    Move source files to src/

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 set -ex
 
 
-gcc -Wall -Wextra -I./extern/build/linux/include/ game.c -L./extern/build/linux/lib/ -l:libraylib.a -lm -ldl -lpthread
+gcc -Wall -Wextra -I./extern/build/linux/include/ src/game.c -L./extern/build/linux/lib/ -l:libraylib.a -lm -ldl -lpthread
 
-x86_64-w64-mingw32-gcc -Wall -Wextra -I./extern/build/win/include/ game.c -L./extern/build/win/lib/ -lraylib -lopengl32 -lgdi32 -lwinmm -static  #-mwindows
+x86_64-w64-mingw32-gcc -Wall -Wextra -I./extern/build/win/include/ src/game.c -L./extern/build/win/lib/ -lraylib -lopengl32 -lgdi32 -lwinmm -static  #-mwindows
 
-emcc -o chess.html -Os -Wall -Wextra ./extern/build/web/lib/libraylib.a -I./extern/build/web/include/ game.c -L./extern/build/web/lib/ --shell-file minshell.html -s USE_GLFW=3 -DPLATFORM_WEB
+emcc -o chess.html -Os -Wall -Wextra ./extern/build/web/lib/libraylib.a -I./extern/build/web/include/ src/game.c -L./extern/build/web/lib/ --shell-file src/minshell.html -s USE_GLFW=3 -DPLATFORM_WEB
