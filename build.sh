fa2cdc003cb7f4af505a9be50339facb3fbf024c
Patrik Brosell <patrikbrosell@gmail.com>
2023-10-11 15:57:54 +0000

    Rename raylib/raylib to extern/raylib

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-gcc -Wall -Wextra -I./raylib/build/linux/include/ game.c -L./raylib//build/linux/lib/ -l:libraylib.a -lm -ldl -lpthread
 
-x86_64-w64-mingw32-gcc -Wall -Wextra -I./raylib/build/win/include/ game.c -L./raylib/build/win/lib/ -lraylib -lopengl32 -lgdi32 -lwinmm -static  #-mwindows
 
-emcc -o chess.html -Os -Wall -Wextra ./raylib/build/web/lib/libraylib.a -I./raylib/build/web/include/ game.c -L./raylib/build/web/lib/ --shell-file minshell.html -s USE_GLFW=3 -DPLATFORM_WEB
+gcc -Wall -Wextra -I./extern/build/linux/include/ game.c -L./extern/build/linux/lib/ -l:libraylib.a -lm -ldl -lpthread
+
+x86_64-w64-mingw32-gcc -Wall -Wextra -I./extern/build/win/include/ game.c -L./extern/build/win/lib/ -lraylib -lopengl32 -lgdi32 -lwinmm -static  #-mwindows
+
+emcc -o chess.html -Os -Wall -Wextra ./extern/build/web/lib/libraylib.a -I./extern/build/web/include/ game.c -L./extern/build/web/lib/ --shell-file minshell.html -s USE_GLFW=3 -DPLATFORM_WEB
